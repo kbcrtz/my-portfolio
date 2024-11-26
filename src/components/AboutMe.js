66b6ba9e466c26f852f874e1a825072a0b9c8bb4
Kobe Cortez <kobecortez31@gmail.com>
2024-11-26 12:28:19 -0800

    fully typing plus contact button

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,16 +1,17 @@
 import React from "react";
+import TypewriterEffect from "./TypewriterEffect";
 
 const AboutMe = () => {
+    const aboutTextOne = 'Hi, I’m Kobe, a sophomore at Pacific Lutheran University with a passion for music, nature, and cars. I enjoy exploring the outdoors, working on cars, and immersing myself in creative and technical challenges. \n \nCurrently, I’m pursuing a degree in Computer Science, and I aspire to become a Software Engineer. I’m eager to build innovative and impactful software solutions that make a difference. My goal is to continuously learn, grow, and contribute to exciting projects in the tech industry.'
     return (
         <section id="AboutMe" style={styles.section}>
             <h2 style={styles.header}>About Me</h2>
             <div style={styles.text}>
-                <p>
-                    Hi, I’m Kobe, a sophomore at Pacific Lutheran University with a passion for music, nature, and cars. I enjoy exploring the outdoors, working on cars, and immersing myself in creative and technical challenges.
-                </p>
-                <p>
-                    Currently, I’m pursuing a degree in Computer Science, and I aspire to become a Software Engineer. I’m eager to build innovative and impactful software solutions that make a difference. My goal is to continuously learn, grow, and contribute to exciting projects in the tech industry.
-                </p>
+          <TypewriterEffect
+            text={aboutTextOne}
+            speed={10}
+            fontFamily="'Fira Code', monospace"
+          />
             </div>
         </section>
     );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+function composeEmail(){
+    const email = 'kobecortez31@gmail.com';
+    const subject = 'Hello Kobe';
+    const body = 'I would like to connect with you.';
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+}
+
 const Contact = () =>{
     return(
         <section id='contact'styles={styles.section} >
             <h4 style={styles.text}>Contact Me</h4>
-            <a href='https://youtu.be/dQw4w9WgXcQ?si=1w-61R8u3GUqTkxV' ><button>click me</button></a>
+            <button onClick={composeEmail}>click me</button>
             <div>
                 <p>    </p>
             </div>
         </section>
     );
 };
+
 const styles ={
     text: {
         fontFamily: "'Fira Code', monospace",
